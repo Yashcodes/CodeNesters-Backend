4d39ebe27c222d9b9370683d80a2952c311b209b
Yash Parihar <thakuryashparihar7089@gmail.com>
2023-08-29 23:18:05 +0530

    added .env file and app.js | Created listening function

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const port = 5000;
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello Express...!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${port}`);
 });
