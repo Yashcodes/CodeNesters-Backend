50b3a0f0538776e589af24274772ead1d00a494e
Yash Parihar <thakuryashparihar7089@gmail.com>
2023-09-07 09:37:04 +0530

    Require sign in middleware created

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+module.exports.requireSignIn = async (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Please authenticate user using a valid token",
+    });
+  }
+
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Please authenticate user using a valid token",
+    });
+  }
+};
