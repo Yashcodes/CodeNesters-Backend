0d157e407199c6c319be84edba779feb037f8742
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-02-06 13:15:22 +0530

    Added aws  s3 route for uploading user profile

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -1,12 +1,50 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const {
   userProfileController,
 } = require("../controllers/userProfileController");
+const { getObjectURL, putObjectURL } = require("./services/s3.js");
 const { requireSignIn } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/user-profile", requireSignIn, userProfileController);
+// router.post("/user-profile", requireSignIn, userProfileController);
+
+//? ROUTE 1: Route for getting putObject Url
+router.post("/putObjectUrl", requireSignIn, async (req, res) => {
+  try {
+    const url = await putObjectURL(req.body.fileName, req.body.contentType);
+
+    res.status(200).json({
+      success: true,
+      message: "Put Object URL Generated Successfully",
+      url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error in generating Put Object URL",
+      url,
+    });
+  }
+});
+
+//? ROUTE 2: Route for getting getObject Url
+router.post("/getObjectUrl", requireSignIn, async (req, res) => {
+  try {
+    const url = await getObjectURL(req.body.key);
+
+    res.status(200).json({
+      success: true,
+      message: "Get Object URL Generated Successfully",
+      url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error in generating Get Object URL",
+      url,
+    });
+  }
+});
 
 module.exports = router;
