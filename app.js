2457eeb832ea27487f8ff9fc1931dd47e0c976db
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-04-12 21:26:22 +0530

    Removed extra configuration in app.js cors headers

--- app.js
+++ app.js
@@ -22,20 +22,6 @@ const app = express();
 //! Connecting to MongoDB Database
 mongoConnect();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, Content-Type, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
 //! Using cors
 app.use(cors());
 
