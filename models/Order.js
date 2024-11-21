efc84f6f6c213582b740a83ab75598e7d6769ef4
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-11-21 20:56:28 +0530

    Added timestamps in Order model

--- models/Order.js
+++ models/Order.js
@@ -1,34 +1,37 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  courses: [
-    {
-      _id: false,
-      courseId: {
-        type: mongoose.Types.ObjectId,
-        ref: "Course",
-        required: true,
-      },
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-  ],
-  razorpay_payment_id: {
-    type: String,
-    required: true,
+    courses: [
+      {
+        _id: false,
+        courseId: {
+          type: mongoose.Types.ObjectId,
+          ref: "Course",
+          required: true,
+        },
+      },
+    ],
+    razorpay_payment_id: {
+      type: String,
+      required: true,
+    },
+    razorpay_order_id: {
+      type: String,
+      required: true,
+    },
+    razorpay_signature: {
+      type: String,
+      required: true,
+    },
   },
-  razorpay_order_id: {
-    type: String,
-    required: true,
-  },
-  razorpay_signature: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 OrderSchema.index({ userId: 1 });
 
