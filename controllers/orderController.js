53d58596ee3aa5ff12e9cfebdeab8ba7adb0ac3a
Yash Parihar <thakuryashparihar7089@gmail.com>
2024-12-04 22:35:26 +0530

    New route added for getting only a single order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -19,3 +19,33 @@ module.exports.getAllOrdersController = async (req, res) => {
     });
   }
 };
+
+module.exports.getOrderDetailsController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    const order = await Order.find({ _id: id, userId })
+      .select("-razorpay_signature")
+      .select("-userId")
+      .populate({
+        path: "courses.courseId",
+        model: "Course",
+        select:
+          "-slug -courseCategory -courseCategoryName -imagePublicId -courseRating",
+      });
+
+    res.status(200).json({
+      success: true,
+      message: "Order Fetched Successfully",
+      order,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { requireSignIn } = require("../middlewares/authMiddleware");
-const { getAllOrdersController } = require("../controllers/orderController");
+const { getAllOrdersController, getOrderDetailsController } = require("../controllers/orderController");
 
 const router = express.Router();
 
-router.get("/get-all-orders", requireSignIn, getAllOrdersController)
+router.get("/get-all-orders", requireSignIn, getAllOrdersController);
+
+router.get("/get-order/:id", requireSignIn, getOrderDetailsController);
 
 module.exports = router;
